bc725c6e68c2d6d6557945017af27696ea0d58c6
Theo Khalil <theokhalil0@icloud.com>
2021-07-21 05:36:00 +0300

    fixes

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-const fs = require("fs");
-const path = require("path");
-require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const HttpError = require("./models/http-error");
-const bookRoutes = require("./routes/book-routes");
-const usersRoutes = require("./routes/users-routes");
-const conversationRoute = require("./routes/conversations");
-const messageRoute = require("./routes/messages");
+const HttpError = require('./models/http-error');
+const bookRoutes = require('./routes/book-routes');
+const usersRoutes = require('./routes/users-routes');
+const conversationRoute = require('./routes/conversations');
+const messageRoute = require('./routes/messages');
 
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-mongoose.set("useCreateIndex", true);
+mongoose.set('useCreateIndex', true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(
+  '/uploads/images',
+  express.static(path.join('uploads', 'images'))
+);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-Width, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE'
+  );
   next();
 });
 
-app.use("/api/books", bookRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/conversations", conversationRoute);
-app.use("/api/messages", messageRoute);
+app.use('/api/books', bookRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messages', messageRoute);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -52,20 +58,22 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({
+    message: error.message || 'An unknown error occurred!',
+  });
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   // console.log("a user connected :D");
-  socket.on("chat message", (msg) => {
+  socket.on('chat message', (msg) => {
     console.log(msg);
-    io.emit("chat message", msg);
+    io.emit('chat message', msg);
   });
 });
 
 mongoose
   .connect(
-    "mongodb://localhost:27017/book-app",
+    'mongodb://localhost:27017/book-app',
     // `mongodb://${process.env.CONNECTION_NAME}:27017,${process.env.CONNECTION_NAME}:27018,${process.env.CONNECTION_NAME}:27019/book-app?replicaSet=rs`,
     {
       useUnifiedTopology: true,
@@ -73,8 +81,8 @@ mongoose
     }
   )
   .then(() => {
-    server.listen(5000);
-    console.log("server running");
+    server.listen(process.env.PORT || 5000);
+    console.log('server running');
   })
   .catch((err) => {
     console.log(err);
