f5a208d333c963ec5e929300a47ccb6be9c544ad
Ghaadyy <ghadyyi@gmail.com>
2021-07-22 18:47:31 +0300

    fixed email design

--- configs/transport.js
+++ configs/transport.js
@@ -18,11 +18,24 @@ module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
       from: user,
       to: email,
       subject: "Please confirm your account",
-      html: `<h1>Email Confirmation</h1>
-        <h2>Hello ${name}</h2>
-        <p>Thank you for joining us. Please confirm your email by clicking on the following link</p>
-        <p>${confirmationCode}</p>
-        </div>`,
+      html: `<div
+        style="
+          text-align: center;
+          background-color: #ffc300;
+          text-decoration: none;
+          font-family: Arial, Helvetica, sans-serif;
+          padding: 1rem;
+        "
+      >
+        <div style="background-color: white; padding: 5rem">
+          <h1 style="margin: 0; color: black">Hello ${name}!</h1>
+          <p style="color: black">
+            Thank you for joining us. Please confirm your email by entering the
+            following code in the app to get in!
+          </p>
+          <b style="font-size: xx-large">${confirmationCode}</b>
+        </div>
+      </div>`,
     })
     .catch((err) => console.log(err));
 };
