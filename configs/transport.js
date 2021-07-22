52f29db4b477d4d05b5f04e7a359307b6c395a4e
Ghaadyy <ghadyyi@gmail.com>
2021-07-22 21:00:33 +0300

    email design fixed

--- configs/transport.js
+++ configs/transport.js
@@ -19,23 +19,36 @@ module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
       to: email,
       subject: "Please confirm your account",
       html: `<div
-        style="
-          text-align: center;
-          background-color: #ffc300;
-          text-decoration: none;
-          font-family: Arial, Helvetica, sans-serif;
-          padding: 1rem;
-        "
-      >
-        <div style="background-color: white; padding: 5rem">
-          <h1 style="margin: 0; color: black">Hello ${name}!</h1>
-          <p style="color: black">
-            Thank you for joining us. Please confirm your email by entering the
-            following code in the app to get in!
-          </p>
-          <b style="font-size: xx-large">${confirmationCode}</b>
+          style="
+            text-align: center;
+            background-color: #ffc300;
+            text-decoration: none;
+            font-family: Arial, Helvetica, sans-serif;
+            padding: 1rem;
+          "
+        >
+          <div
+            style="
+              background-color: white;
+              padding: 5rem;
+            "
+          >
+            <h1 style="margin: 0; color: black">Hello ${name}!</h1>
+            <p style="color: black">
+              Thank you for joining us. Please confirm your email by entering the
+              following code in the app to get in!
+            </p>
+            <b style="font-size: xx-large">${confirmationCode}</b><br>
+            <em style="font-size: small; color: rgb(139, 139, 139); margin-top: 2rem">
+              When exchanging or selling books, it is advised to meet in a public area
+              for safety measures.
+            </em><br>
+            <em style="font-size: small; color: rgb(139, 139, 139)">
+              Check the price lists before purchasing any book.
+            </em>
+          </div>
         </div>
-      </div>`,
+        `,
     })
     .catch((err) => console.log(err));
 };
